5df73be15dcd922fc1f96af66680dd47a754c7b7
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-05-14 15:09:27 -0400

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,22 +1,23 @@
 public class BinaryTree {
             TreeNode Treeroot;
 
-                public void printPreorderTraversal() {
-                        printPreorderTraversal(Treeroot);
-                        }
+     public void printPreorderTraversal() {
+                printPreorderTraversal(Treeroot);
+                      }
 
-                public BinaryTree() {
-                Treeroot = null;
+     public BinaryTree() {
+             Treeroot = null;
                   }
 
-                public void printPreorderTraversal(TreeNode Tnode){
+     public void printPreorderTraversal(TreeNode Tnode){
                         if (Tnode == null)
                                 return;
+        
          System.out.print(Tnode.key + " ");
          printPreorderTraversal(Tnode.left);
 
          printPreorderTraversal(Tnode.right);
-}
+                      }
     
 
 }
