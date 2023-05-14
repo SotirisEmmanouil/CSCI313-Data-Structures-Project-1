e76cfa8743a81e33b5da4f543c887ed7abb044af
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-05-14 14:30:26 -0400

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,22 @@
+public class BinaryTree {
+            TreeNode Treeroot;
+
+                public void printPreorderTraversal() {
+                        printPreorderTraversal(Treeroot);
+                        }
+
+                public BinaryTree() {
+                Treeroot = null;
+                  }
+
+                public void printPreorderTraversal(TreeNode Tnode){
+                        if (Tnode == null)
+                                return;
+         System.out.print(Tnode.key + " ");
+         printPreorderTraversal(Tnode.left);
+
+         printPreorderTraversal(Tnode.right);
+}
+    
+
+}
