37713c3c1afa1677718c0404d9ae0d0d6e5f3d2b
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-05-14 18:35:11 -0400

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,19 +1,19 @@
 public class BinaryTree {
-            TreeNode Treeroot;
+            TreeNode Treeroot;    //create an treenode called treeroot
 
-     public void printPreorderTraversal() {
-                printPreorderTraversal(Treeroot);
+     public void printPreorderTraversal() {                 
+               printPreorderTraversal(Treeroot);
                       }
 
-     public BinaryTree() {
+     public BinaryTree() {          //BinaryTree constructor which sets the root to null
              Treeroot = null;
                   }
 
-     public void printPreorderTraversal(TreeNode Tnode){
+     public void printPreorderTraversal(TreeNode Tnode){            
                         if (Tnode == null)
                                 return;
         
-         System.out.print(Tnode.key + " ");
+         System.out.print(Tnode.key + " ");     //recursively print the nodes of the binary tree
          printPreorderTraversal(Tnode.left);
          printPreorderTraversal(Tnode.right);
                       }
